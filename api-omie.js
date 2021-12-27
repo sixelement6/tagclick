5aa764018a473bfff3f3cf9d11ebbec14c75a059
sixelement6 <guilhermefbachiega@gmail.com>
2021-12-27 16:44:00 -0300

    First Commit Tag_v2

---
+++ api-omie.js
@@ -0,0 +1,26 @@
+async function getPost() {
+  const resp = await fetch(
+    "https://app.omie.com.br/api/v1/geral/produtos/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: ({
+        call: "ListarProdutos",
+        app_key: "1534719465279",
+        app_secret: "05688d0ab54686f019cdf187089f6721",
+        param: [
+          {
+            pagina: 1,
+            registros_por_pagina: 500,
+            apenas_importado_api: "N",
+            filtrar_apenas_omiepdv: "N",
+          },
+        ],
+      }),
+    }
+  );
+
+  console.log(resp.json());
+}
